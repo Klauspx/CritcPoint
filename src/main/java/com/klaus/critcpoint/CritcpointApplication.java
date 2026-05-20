cee1980d2e57db7216d70f80af3d8d07e4bed7f7
Nadson Klaus <klausplima@gmail.com>
2026-05-19 21:41:34 -0300

    Initial commit

---
+++ src/main/java/com/klaus/critcpoint/CritcpointApplication.java
@@ -0,0 +1,13 @@
+package com.klaus.critcpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CritcpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CritcpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klaus/critcpoint/CritcpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klaus.critcpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CritcpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
