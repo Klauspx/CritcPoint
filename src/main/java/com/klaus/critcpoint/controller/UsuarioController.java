3d4accd5189fbee899cfbd9f22cd5c4d291d9a53
Nadson Klaus <klausplima@gmail.com>
2026-05-21 23:01:36 -0300

    feat: adiciona rotas de GET, DELETE e PUT para usuario

--- src/main/java/com/klaus/critcpoint/controller/UsuarioController.java
+++ src/main/java/com/klaus/critcpoint/controller/UsuarioController.java
@@ -3,10 +3,9 @@ package com.klaus.critcpoint.controller;
 import com.klaus.critcpoint.model.Usuario;
 import com.klaus.critcpoint.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -17,5 +16,24 @@ public class UsuarioController {
     @PostMapping("/usuarios")
     public Usuario salvarUsuario (@RequestBody Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    @GetMapping("/usuarios")
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    public void deleteUsuario(@PathVariable Long id) {
+        usuarioRepository.deleteById(id);
+    }
+
+    @PutMapping("/usuarios/{id}")
+    public Usuario atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioNovo) {
+        Usuario usuarioExistente = usuarioRepository.findById(id).get();
+        usuarioExistente.setNome(usuarioNovo.getNome());
+        usuarioExistente.setEmail(usuarioNovo.getEmail());
+        usuarioExistente.setSenha(usuarioNovo.getSenha());
+        return usuarioRepository.save(usuarioExistente);
     }
 }
